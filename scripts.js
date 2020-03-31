55e2bb5c7ffa8729308969c96ae914b3ddebef26
Travis Surgnier <surgie@gmail.com>
2020-03-31 19:22:22 -0400

    notes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
 function runPrediction(save) {
-    let query = (name) => $(save).find(name).text();
+    let query = (name) => $(save).children('SaveGame').children(name)[0].text();
     let queryAsNumber = (name) => parseInt(query(name), 10);
+
+    // TODO bah this is bad, redo it again?
+    // changge to fastXmlParser? https://www.npmjs.com/package/fast-xml-parser
+    // https://cdnjs.com/libraries/fast-xml-parser
+    // can then drop jquery?
+
+    // https://github.com/NaturalIntelligence/fast-xml-parser#readme (XML to JSON)
+    // var result = parser.validate(xmlData);
+    // if (result !== true) console.log(result.err);
+    // var jsonObj = parser.parse(xmlData);
 
     const uniqueId = queryAsNumber('uniqueIDForThisGame');
     const lostbooksFound = queryAsNumber('lostBooksFound');
