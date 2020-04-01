84d13bb25fecc03f9d99f6a4507c053c4db8d874
Travis Surgnier <surgie@gmail.com>
2020-03-31 21:28:44 -0400

    Progress, can predict the ancient doll!

--- scripts.js
+++ scripts.js
@@ -41,13 +41,14 @@ class GameLocation {
 }
 
 class Predictor {
-    constructor(save) {
+    constructor(save, objectInformation) {
         this.uniqueId = save.uniqueIDForThisGame;
         this.booksFound = save.lostBooksFound;
         this.daysPlayed = save.player.stats.DaysPlayed;
         this.currentSeason = save.currentSeason;
 
         this.locations = save.locations.GameLocation.filter(x => !!x.objects).map(x => new GameLocation(x));
+        this.objectInformation = objectInformation;
     }
 
     get locationsWithArtifacts() {
@@ -55,19 +56,62 @@ class Predictor {
     }
 
     dig(locationName, xPos, yPos) {
+        var objectIndex = -1;
 
+        var rand = new CSRandom(xPos * 2000 + yPos + this.uniqueId / 2 + this.daysPlayed);
+
+        for (var i = 0; i < this.objectInformation.length; i++) {
+            var odds = this.objectInformation[i].OddsPerLocation[locationName];
+
+            if (!odds) {
+                continue;
+            }
+
+            if (rand.NextDouble() < odds) {
+                objectIndex = this.objectInformation[i].ObjectId;
+                break;
+            }
+        }
+
+        if (rand.NextDouble() < 0.2 && locationName !== 'Farm') {
+            objectIndex = 102;
+        }
+
+        if (objectIndex === 102 && this.booksFound >= 21) {
+            objectIndex = 770;
+        }
+
+        if (objectIndex !== -1) {
+            return objectIndex;
+        } else if (this.currentSeason === 'winter' && rand.NextDouble() < 0.5 && locationName !== 'Desert') {
+            if (rand.NextDouble() < 0.4) {
+                return 416;
+            } else {
+                return 412;
+            }
+        } else {
+            if (this.currentSeason === 'spring' && rand.NextDouble() < 1.0 / 16.0) {
+                if (locationName !== 'Desert' && locationName !== 'Beach') {
+                    // To determine amount of rice: random.Next(2, 6)
+                    return 273;
+                }
+            }
+
+            // TODO: continue from line 123 of Predictor.cs
+        }
     }
 
     print() {
         this.locationsWithArtifacts.forEach(location => {
             location.print();
             location.artifactSpots.forEach(artifactSpot => artifactSpot.print());
+            location.artifactSpots.forEach(artifactSpot => console.log('    ' + this.dig(location.name, artifactSpot.xPos, artifactSpot.yPos)));
         });
     }
 }
 
-function runPrediction(save) {
-    const predictor = new Predictor(save);
+function runPrediction(save, objectInformation) {
+    const predictor = new Predictor(save, objectInformation);
     predictor.print();
 
     const predictions = document.getElementById('predictions');
@@ -83,7 +127,7 @@ function readSaveFile(event) {
             var result = parser.validate(e.target.result);
             if (result !== true) console.log(result.err);
             var jsonObj = parser.parse(e.target.result);
-            runPrediction(jsonObj.SaveGame);            
+            runPrediction(jsonObj.SaveGame, window.objectInformation);            
         }
     }
 
