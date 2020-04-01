89e3aaf1d9e96f101f8013478b583155bde170ec
Travis Surgnier <surgie@gmail.com>
2020-04-01 18:43:53 -0400

    Algorithm mostly good, except for quantity and secret notes?

--- predictor.js
+++ predictor.js
@@ -1,6 +1,7 @@
 import { CSRandom } from './cs-random.js';
 import { GameLocation } from './models.js';
 import { objectInformation } from './data/objectInformation.js';
+import { secondHalfOfDigging } from './data/locations.js';
 
 export class Predictor {
     constructor(save) {
@@ -58,15 +59,23 @@ export class Predictor {
                 }
             }
 
-            // TODO: continue from line 123 of Predictor.cs
+            objectIndex = secondHalfOfDigging(locationName, rand, this.booksFound);
+
+            if (objectIndex === -1) {
+                objectIndex = 330;
+            }
+
+            return objectIndex;
         }
     }
 
     print() {
         this.locationsWithArtifacts.forEach(location => {
-            location.print();
-            location.artifactSpots.forEach(artifactSpot => artifactSpot.print());
-            location.artifactSpots.forEach(artifactSpot => console.log('    ' + this.dig(location.name, artifactSpot.xPos, artifactSpot.yPos)));
+            location.artifactSpots.forEach(as => {
+                const objectIndex = this.dig(location.name, as.xPos, as.yPos);
+                const object = objectInformation.find(x => x.ObjectId === objectIndex);
+                console.log(`    digging at ${location.name} ${as.xPos} ${as.yPos} - expecting ${object.DisplayName}`);
+            })
         });
     }
 }
